e7025299094cdb7cc36e64348e138333148b18e0
Richard Bone <rjonbone@gmail.com>
2016-04-03 22:21:47 +1000

    Implemented some basic tests for currency::from_code

--- tests/lib.rs
+++ tests/lib.rs
@@ -14,3 +14,17 @@ fn test_japan_formatting() {
     let money = Money::new(12345, currency::JPY);
     assert_eq!("JPY¥12345", money.format());
 }
+
+#[test]
+fn test_create_currency_from_code() {
+    let currency = currency::from_code("USD").unwrap();
+    assert_eq!(currency.code(), "USD");
+    assert_eq!(currency.symbol(), '$');
+    assert_eq!(currency.divisor(), 100);
+}
+
+#[test]
+fn test_create_bad_currency_returns_err() {
+    let currency = currency::from_code("ABC");
+    assert!(currency.is_err());
+}
